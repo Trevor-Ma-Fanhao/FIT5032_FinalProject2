3ec091d3f77b87250e2460d4a51c4b4e57b88fa0
Trevor <932952939@qq.com>
2023-08-11 16:31:27 +0800

    添加项目文件。

---
+++ FIT5032_FinalProject2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_FinalProject2.Startup))]
+namespace FIT5032_FinalProject2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
