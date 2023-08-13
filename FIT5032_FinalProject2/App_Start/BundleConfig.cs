deb41e4616e94f4dbaae2d5536674945a21a0b74
Trevor <932952939@qq.com>
2023-08-13 20:08:51 +0800

    location.js的修改

--- FIT5032_FinalProject2/App_Start/BundleConfig.cs
+++ FIT5032_FinalProject2/App_Start/BundleConfig.cs
@@ -22,6 +22,10 @@ namespace FIT5032_FinalProject2
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            // I added the location.js to the bundle called mapbox.
+            bundles.Add(new ScriptBundle("~/bundles/mapbox").Include(
+                       "~/Scripts/location.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
